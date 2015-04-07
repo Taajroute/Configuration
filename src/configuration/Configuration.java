323fc117f339607372f72c210b634112a3954fef
sadi <sadiahcene84@yahoo.fr>
2015-04-07 17:19:31 +0200

    implémentation de la classe Configuration.

--- src/configuration/Configuration.java
+++ src/configuration/Configuration.java
@@ -15,19 +15,49 @@ import java.util.logging.Logger;
  */
 public class Configuration {
 
+  int Id_Mode;
+  String Seed ; 
+  String Start ; 
+  String End; 
+  String Max;
+  
+  String Gtin;
+  String Gtin_Size ;
+  
+  String Date_Fabrication; 
+  String Date_Experation;
+  
+  String Batch;
+  String Batch_Size;
+  
+  String Hidden_Message ;
+  String Hidden_Message_Size ;
+  
+  String Code_Barres_Type;
+ 
+  
+
+  @Override
+  public String toString() {
+    return "Configuration mode "+Id_Mode + 
+           "\n Seed "+Seed+
+           "\n Start "+Start+
+           "\n End "+End+
+           "\n Max "+Max+
+           "\n Gtin "+Gtin+ "\tSize="+Gtin_Size+
+           "\n Date Fabrication "+Date_Fabrication+
+           "\n Date Experation "+Date_Experation+
+           "\n Batch "+Gtin+ "\tSize="+Batch_Size+
+           "\n Hidden Message "+Hidden_Message+ "\tSize="+Hidden_Message_Size+
+           "\n Code Barres Type "+Code_Barres_Type;
+  }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        try 
-        {
-            System.out.println("Hello  ...... ");
-            System.in.read();
-        } catch (IOException ex) {
-            Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Configuration  conf = new Configuration() ;
+        System.out.println(conf);
+        System.out.println("Fin de programme  ...... ");
     }
     
 }
